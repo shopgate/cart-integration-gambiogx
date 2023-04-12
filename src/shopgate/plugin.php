4186559b91487533a2b5aa291d72c586ced31d40
Timo Ebel <timo.ebel@shopgate.com>
2023-04-12 14:39:05 +0200

    v2.9.58

--- src/shopgate/plugin.php
+++ src/shopgate/plugin.php
@@ -21,7 +21,7 @@
  * @copyright Shopgate Inc
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, Version 2.0
  */
-define("SHOPGATE_PLUGIN_VERSION", "2.9.57");
+define("SHOPGATE_PLUGIN_VERSION", "2.9.58");
 
 /**
  * GambioGX Plugin for Shopgate
